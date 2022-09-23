e32dd2299714ba0d925b25907ec7a45183e8a203
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-23 12:36:25 -0300

    return submit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import data from "./data";
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <section className="section-center">
       <h3>tired of boring lorem ipsum ?</h3>
-      <form className="lore-form"></form>
+      <form className="lore-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+      </form>
     </section>
   );
 }
